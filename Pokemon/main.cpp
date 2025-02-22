b2133dd1d019172bea8a16b3b1ee60e2b7cbc31f
Nirbhau0901 <nirbhausingh73@gmail.com>
2025-02-22 12:49:49 +0530

    Updated main function
    Added introduction dialogue and Pokemon choice along with input and confirmation message.

--- Pokemon/main.cpp
+++ Pokemon/main.cpp
@@ -1,20 +1,43 @@
 #include <iostream>
 using namespace std;
 
-int main() 
+int main()
 {
-    string player_name;
-
-    cout << "Trainer! Before you embark on our journey, the Pokemon world needs to know who you are!" << endl;
-    cout << " " << endl;
-    cout << "Time to step up and tell us your name." << endl;
-    cout << "And remember, a great name is the start of every great adventure!" << endl;
+    cout << "Ah, Trainer!" << endl;
+    cout << "I am Professor Oak." << endl;
+    cout << "Welcome to the world of Pokemon!" << endl;
+    cout << "Today is a momentous day, you'll be choosing your very first Pokemon." << endl;
+    cout << "Every great trainer remembers this moment for the rest of their lives." << endl;
+    cout << "So, Choose wisely, young one!" << endl;
+    cout << "You can choose one of the Following Pokemon:" << endl;
+    cout << "1. Balbasaur\n2. Chramander\n3. Squirtle" << endl;
+    cout << "Which Pokemon would you like to choose. Enter number: " ;
     
-    cout << "Enter Your Name:";
-    cin >> player_name;
+    int choice;
+    cin >> choice;
+    
+    if (choice == 1)
+    {
+        cout << "You chose Balbasaur! A Wise Choice." << endl;
+    }
+    else if (choice == 2)
+    {
+        cout << "You chose Charmander! A Fiery Choice." << endl;
+    }
+    else if (choice == 3)
+    {
+        cout << "You Chose Squirtle! A Cool Choice." << endl;
+    }
+    else
+    {
+        cout << "Invalid Choice, Please restart the Game." << endl;
+    }
 
-    cout << "Great Start" << player_name << "Looks like you've mastered the opening of every code adventure. Keep this up, and you'll be a coding master in no time!" << endl;
-
+    cout << choice << endl;
+    cout << "But beware Trainer" << endl;
+    cout << "Your journey is just about to unfload" << endl;
+    cout << "Now let's see if you've got what it takes to keep going!" << endl;
+    cout << "Good Luck, and remember choose wisely" << endl;
 
     return 0;
 }
