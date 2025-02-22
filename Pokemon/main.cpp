56368d11515899d0ed7fa1fc4ecfa820aee5575f
Nirbhau0901 <nirbhausingh73@gmail.com>
2025-02-22 12:15:57 +0530

    Added main function
    Added main function, player name variable and input for the player name

--- Pokemon/main.cpp
+++ Pokemon/main.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
+using namespace std;
 
-int main() {
+int main() 
+{
+    string player_name;
+
+    cout << "Trainer! Before you embark on our journey, the Pokemon world needs to know who you are!" << endl;
+    cout << " " << endl;
+    cout << "Time to step up and tell us your name." << endl;
+    cout << "And remember, a great name is the start of every great adventure!" << endl;
+    
+    cout << "Enter Your Name:";
+    cin >> player_name;
+
+    cout << "Great Start" << player_name << "Looks like you've mastered the opening of every code adventure. Keep this up, and you'll be a coding master in no time!" << endl;
+
 
     return 0;
 }
